78b47fe9e487db3e43cc4a193f16402c3db7f800
mekows <1361394078@qq.com>
2018-11-03 14:02:19 +0800

    第一次使用git管理代码版本

--- src/main/scala/SparkMekows/WordCount.scala
+++ src/main/scala/SparkMekows/WordCount.scala
@@ -10,6 +10,5 @@ object WordCount {
     val textFile = sc.textFile(inputFile)
     val wordCount = textFile.flatMap(line => line.split("=>")).map(word => (word, 1)).reduceByKey((a, b) => a + b)
     wordCount.foreach(println)
-
   }
 }
